da8cccc3df9abf8cf74f13af3cb791d5c073b1aa
tadi-karuma <77990278+tadi-karuma@users.noreply.github.com>
2025-04-28 16:39:16 +0900

    Update plugins.zsh

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,6 +1,6 @@
 
-zi ice wait'!0'; zi light zsh-users/zsh-autosuggestions
-zi ice wait'!0'; zi light zsh-users/zsh-completions
-zi ice wait'!0'; zi light zdharma/fast-syntax-highlighting
-zi ice wait'!0'; zi light zdharma/history-search-multi-word
+#zi ice wait'!0'; zi light zsh-users/zsh-autosuggestions
+#zi ice wait'!0'; zi light zsh-users/zsh-completions
+#zi ice wait'!0'; zi light zdharma/fast-syntax-highlighting
+#zi ice wait'!0'; zi light zdharma/history-search-multi-word
 
