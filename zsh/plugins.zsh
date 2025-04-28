665f0e2f1b392547cb2277fd589b1206252cbc91
tadi-karuma <77990278+tadi-karuma@users.noreply.github.com>
2025-04-28 14:35:05 +0900

    Update plugins.zsh

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,5 +1,4 @@
 
-zi ice depth=1;	 zi light romkatv/powerlevel10k
 zi ice wait'!0'; zi light zsh-users/zsh-autosuggestions
 zi ice wait'!0'; zi light zsh-users/zsh-completions
 zi ice wait'!0'; zi light zdharma/fast-syntax-highlighting
